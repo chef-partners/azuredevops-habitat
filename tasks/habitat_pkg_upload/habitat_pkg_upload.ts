5dc68efdf9a1379f4d523591a233ca5373321b65
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-06-11 17:42:52 +0100

    Updated task so that it can handle wildcards in the path
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/habitat_pkg_upload/habitat_pkg_upload.ts
+++ tasks/habitat_pkg_upload/habitat_pkg_upload.ts
@@ -5,6 +5,11 @@ import * as tl from "vsts-task-lib/task";
 // Import common tasks
 import * as inputs from "./common/inputs";
 
+// Import the glob library so that wildcard paths can be resolved
+import * as glob from "glob"
+
+import * as fs from "fs-extra"
+
 import {sprintf} from "sprintf-js";
 
 async function run() {
@@ -12,15 +17,29 @@ async function run() {
     // get the parameters
     let params = inputs.parse(process, tl)
 
-    // build up the command that needs to be run
-    let args = sprintf("pkg upload %s", params["habitatPackagePath"])
+    // find the package file that has been referenced
+    let package_files = glob.sync(params["habitatPackagePath"])
 
-    // execute the upload to the depot
-    try {
-        let exit_code = await tl.tool("/tmp/hab").line(args).exec()
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message)
-    }    
+    // iterate around the package files that have been found
+    for (var i = 0; i < package_files.length; i ++) {
+
+        // ensure that the file exists
+        if (fs.exists(package_files[0])) {
+
+            // build up the command that needs to be run
+            let args = sprintf("pkg upload %s", package_files[0])
+
+            // execute the upload to the depot
+            try {
+                let exit_code = await tl.tool("/tmp/hab").line(args).exec()
+            } catch (err) {
+                tl.setResult(tl.TaskResult.Failed, err.message)
+            }    
+
+        }
+    }
+
+
 }
 
 run();
