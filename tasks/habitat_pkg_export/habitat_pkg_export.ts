76bb43a92a7f91578f58e655b1446a8ec4f3ea6f
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2018-06-24 16:03:05 +0100

    Updated task to export from a hart file
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/habitat_pkg_export/habitat_pkg_export.ts
+++ tasks/habitat_pkg_export/habitat_pkg_export.ts
@@ -2,6 +2,9 @@
 // Import tasks from vsts
 import * as tl from "vsts-task-lib/task";
 
+// Import the glob library so that wildcard paths can be resolved
+import * as glob from "glob";
+
 // Import common tasks
 import * as task from "./common/TaskConfiguration";
 
@@ -15,30 +18,34 @@ async function run() {
     // get the parameters and default settings
     let required = [
         "habitatExportFormat",
-        "habitatExportVersion",
-        "habitatExportName",
-        "habitatUseSudo"
+        "habitatPackagePath"
     ];
 
-    let params = await taskParameters.getTaskParameters(required, "habitatOrigin");
+    let params = await taskParameters.getTaskParameters(required);
 
-    // build up the command to run
-    // this needs to be adjusted if useSudo is true
-    let cmd = params.paths["habitat"];
-    let args = sprintf("pkg export %s %s/%s/%s", params.exportFormat, params.originName, params.exportName, params.exportVersion);
+    // find the package file that has been referenced
+    let package_files = glob.sync(params.packagePath);
 
-    if (params.useSudo) {
-        args = sprintf("%s %s", cmd, args);
-        cmd = "sudo";
-    }
+    // iterate around the package files that have been found
+    for (let i = 0; i < package_files.length; i ++) {
 
-    // if in debug mode output the command that is being executed
-    tl.debug(sprintf("Command: %s", cmd, args));
+        // ensure that the file exists
+        if (tl.exist(package_files[0])) {
 
-    try {
-        let exit_code = await tl.tool(cmd).line(args).exec();
-    } catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+            // build up the command that needs to be run
+            let cmd = params.paths["habitat"];
+            let args = sprintf("pkg export %s %s", params.exportFormat, package_files[0]);
+
+            // if in debug mode output the command being executed
+            tl.debug(sprintf("Command: %s %s", cmd, args));
+
+            // execute the upload to the depot
+            try {
+                let exit_code = await tl.tool(cmd).line(args).exec;
+            } catch (err) {
+                tl.setResult(tl.TaskResult.Failed, err.message);
+            }
+        }
     }
 }
 
