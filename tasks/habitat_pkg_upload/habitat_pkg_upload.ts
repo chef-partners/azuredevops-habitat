5dedec4bfd772e77b921fe053a5ff89bd2266b44
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2018-06-24 16:42:26 +0100

    Added checks to ensure packages have been found
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- tasks/habitat_pkg_upload/habitat_pkg_upload.ts
+++ tasks/habitat_pkg_upload/habitat_pkg_upload.ts
@@ -26,26 +26,33 @@ async function run() {
     let package_files = glob.sync(params.packagePath);
 
     // state how many files are to be exported
-    console.log("Number of packages to be uploaded: %s", String(package_files.length));
+    console.log("Package files path: %s", params.packagePath);
+    console.log("Number of packages to be exported: %s", String(package_files.length));
 
-    // iterate around the package files that have been found
-    for (let i = 0; i < package_files.length; i ++) {
+    // if there are no packages to upload throw an error
+    if (package_files.length === 0) {
+        tl.setResult(tl.TaskResult.Failed, "No packages found for upload");
+    } else {
 
-        // ensure that the file exists
-        if (tl.exist(package_files[0])) {
+        // iterate around the package files that have been found
+        for (let i = 0; i < package_files.length; i ++) {
 
-            // build up the command that needs to be run
-            let cmd = params.paths["habitat"];
-            let args = sprintf("pkg upload %s", package_files[0]);
+            // ensure that the file exists
+            if (tl.exist(package_files[0])) {
 
-            // if in debug mode output the command being executed
-            tl.debug(sprintf("Command: %s %s", cmd, args));
+                // build up the command that needs to be run
+                let cmd = params.paths["habitat"];
+                let args = sprintf("pkg upload %s", package_files[0]);
 
-            // execute the upload to the depot
-            try {
-                let exit_code = await tl.tool(cmd).line(args).exec;
-            } catch (err) {
-                tl.setResult(tl.TaskResult.Failed, err.message);
+                // if in debug mode output the command being executed
+                tl.debug(sprintf("Command: %s %s", cmd, args));
+
+                // execute the upload to the depot
+                try {
+                    let exit_code = await tl.tool(cmd).line(args).exec;
+                } catch (err) {
+                    tl.setResult(tl.TaskResult.Failed, err.message);
+                }
             }
         }
     }
